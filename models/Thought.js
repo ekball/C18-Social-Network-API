143126e29d51c3ddacc62d4e545611c1ace04cd1
ekball <90882418+ekball@users.noreply.github.com>
2022-02-16 20:16:21 -0500

    added the reaction schema into the reactions section in the ThoughtSchema

--- models/Thought.js
+++ models/Thought.js
@@ -21,10 +21,28 @@ const ThoughtSchema = new Schema(
             required: true
         },
         reactions: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Reactions'
-            }
+            // create new reactions
+                {
+                    reactionId: {
+                        type: Schema.Types.ObjectId,
+                        default: () => new Types.ObjectId(),
+                    },
+                    reactionBody: {
+                        type: String,
+                        required: true,
+                        max: 280
+                    },
+                    username: {
+                        type: String,
+                        required: true
+                        },
+                    createdAt: {
+                        type: Date,
+                        default: Date.now,
+                        get: createdAtVal => dateFormat(createdAtVal)
+                    }
+                    
+                }
         ]
     },
     {
