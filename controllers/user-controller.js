9be88997572aa1aeda65420a8df3f7a04a8533f6
ekball <90882418+ekball@users.noreply.github.com>
2022-02-20 12:01:43 -0500

    added the index.js in models, utils folder with date formatter code, and fixed small error in user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,20 +5,20 @@ const userController = {
     // get all users
     getAllUsers(req, res) {
         User.find({})
-        .populate({
-            path: 'thoughts',
-        })
-        .populate({
-            path: 'friends'
-        })
-        .select('-__v')
+            .populate({
+                path: 'thoughts',
+            })
+            .populate({
+                path: 'friends'
+            })
+            .select('-__v')
 
-        .then(dbUserData => res.json(dbUserData))
-      
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
+            .then(dbUserData => res.json(dbUserData))
+        
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
     },
 
     // get one user by id
@@ -78,10 +78,10 @@ const userController = {
     },
 
     // add a friend by updating a particular user
-    addFriend({ req, res }){
+    addFriend({ params }, res){
         User.findOneAndUpdate(
-            { _id: req.params.userId },
-            { $push: { friends: req.params.friendId } },
+            { _id: params.userId },
+            { $push: { friends: params.friendId } },
             { new: true, runValidators: true }
         )
         .then(dbUserData => {
